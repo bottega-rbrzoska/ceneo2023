b3ffdfbd8857a88178f98b26af4c5e160e0d9f11
Rafal Brzoska <rbrzoska@future-processing.com>
2023-09-13 16:46:20 +0200

    directives

--- cng/src/app/test/test.component.ts
+++ cng/src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/models/User';
 
 @Component({
   selector: 'app-test',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent {
-
+  show = false;
+ boxColor="red" 
+  collection: User[] = [
+    { email: 'a@b.pl', name: 'Alojzy', isAdmin: false },
+    { email: 'c@c.pl', name: 'Tomasz', isAdmin: false },
+    { email: 'e@f.pl', name: 'Jarosław', isAdmin: true }
+  ]
+  toggle() {
+    this.show = !this.show;
+  }
 }
